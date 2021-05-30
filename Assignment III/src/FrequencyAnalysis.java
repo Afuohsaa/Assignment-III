b82372cce384b0ec8d03de356a827f32d1f4d7d2
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-30 14:01:53 +0300

    modifying to standards of assignment

--- Assignment III/src/FrequencyAnalysis.java
+++ Assignment III/src/FrequencyAnalysis.java
@@ -5,12 +5,12 @@ public class FrequencyAnalysis {
 	public static void main(String[] args) {
 	
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter the Cipher Text: ");
+		System.out.println("Enter the Cipher Text: \nCipher Text: ");
 		String cipherText = scanner.nextLine();
 		cipherText = cipherText.toUpperCase(); //We just want all text in uppercase
 
 		//Counting of character frequency
-		System.out.println("Character and Frequency are: ");
+		System.out.println("Analysis: ");
 		for(char ch ='A'; ch<='Z'; ch++) {
 			
 			int n = 0;
@@ -22,6 +22,10 @@ public class FrequencyAnalysis {
 			if (n!=0)
 			System.out.print(ch + "->" + n + ", ");	
 		}
+		
+		//Display user menu:
+		Scanner scanner1 = new Scanner(System.in);
+		
 	}
 
 }
