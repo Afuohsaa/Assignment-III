a2e41e2f5b6ea4076af3f0e639a133dd968edab6
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-30 13:17:16 +0300

    Requesting a cipher text input from user

--- Assignment III/src/FrequencyAnalysis.java
+++ Assignment III/src/FrequencyAnalysis.java
@@ -2,7 +2,11 @@
 public class FrequencyAnalysis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the Cipher Text: ");
+		String cipherText = scanner.nextLine();
+		cipherText = cipherText.toUpperCase();
 
 	}
 
