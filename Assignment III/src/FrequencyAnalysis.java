37113d593e57aeca566be152a083380c53640ead
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-05-30 13:29:13 +0300

    creation of our dictionary [A to z] and some formular to perform count

--- Assignment III/src/FrequencyAnalysis.java
+++ Assignment III/src/FrequencyAnalysis.java
@@ -6,8 +6,17 @@ public class FrequencyAnalysis {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the Cipher Text: ");
 		String cipherText = scanner.nextLine();
-		cipherText = cipherText.toUpperCase();
+		cipherText = cipherText.toUpperCase(); //We just want all text in uppercase
 
+		//Counting of character frequency
+		for(char ch ='A'; ch<='Z'; ch++) {
+			
+			int n = 0;
+			for(int i=0; i<cipherText.length(); i++) {
+				
+				
+			}
+		}
 	}
 
 }
