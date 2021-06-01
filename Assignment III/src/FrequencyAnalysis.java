d0caefe905f117b9a48b0339760ec7ac2093988d
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-06-01 22:35:49 +0300

    my codes sems to be okay but in the second analysis, letters with zero frequencies keep occuring

--- Assignment III/src/FrequencyAnalysis.java
+++ Assignment III/src/FrequencyAnalysis.java
@@ -34,30 +34,28 @@ public class FrequencyAnalysis {
 		Option = scanner2.nextInt();
 		
 		if(Option== 1) {
-	
-			//Scanner input = new Scanner(System.in);
-			//System.out.println("Enter the Cipher Text Again: ");
-			//input=scanner;
+
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter the Cipher Text Again: ");
+			String str = input.nextLine();
 			System.out.println("Enter the character to replace");
-			//String str = scanner.nextLine();
-			//System.out.println("Enter the character to replace");
-			char ch = scanner.next().charAt(0);
+			char ch = input.next().charAt(0);
 			System.out.println("Enter the character to be replaced with");
-			char newCh = scanner.next().charAt(0);
-			String str = scanner.nextLine();
+			char newCh = input.next().charAt(0);
+			
 			String newText = str.replace(ch, newCh);
 			System.out.println(newText);
 			newText =newText.toUpperCase();
-			
+
 			for(char ch1 ='A';ch1<='Z';ch1++ ) {
 			int n = 0;
 			for(int j=0; j<newText.length(); j++) {
-				
+
 				if(ch1 == newText.charAt(j))
 					n++;
-				
+	
 			}
-			if(n>0);
+			if(n!=0);
 			System.out.print(ch1 + "->" + n + ", ");	
 		}
 	}
