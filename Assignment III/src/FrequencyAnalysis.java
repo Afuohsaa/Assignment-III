1b31c316727ef91996944edc76ee393cdd9898b9
Nebaval <Nebaval@DESKTOP-7ENT7QP>
2021-06-01 23:18:13 +0300

    final commit. my work is limited as i am still a beginner in java

--- Assignment III/src/FrequencyAnalysis.java
+++ Assignment III/src/FrequencyAnalysis.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class FrequencyAnalysis {
 
 	public static void main(String[] args) {
-	
+
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter the Cipher Text: \nCipher Text: ");
 		String cipherText = scanner.nextLine();
@@ -12,27 +12,27 @@ public class FrequencyAnalysis {
 		//Counting of character frequency
 		System.out.println("Analysis: ");
 		for(char ch ='A'; ch<='Z'; ch++) {
-			
+
 			int n = 0;
 			for(int i=0; i<cipherText.length(); i++) {
-				
+
 				if(ch == cipherText.charAt(i))
 					n++;
 			}
 			if (n!=0)
-			System.out.print(ch + "->" + n + ", ");	
+				System.out.print(ch + "->" + n + ", ");	
 		}
-		
+
 		//Display user menu:
 		Scanner scanner1 = new Scanner(System.in);
 		System.out.println("\n\nOption:\n1) Take Replacement Rule\n2) Exit");
-		
+
 		int Option;
-		
+
 		Scanner scanner2 = new Scanner(System.in);
 		System.out.println("\nEnter 1 or 2: ");
 		Option = scanner2.nextInt();
-		
+
 		if(Option== 1) {
 
 			Scanner input = new Scanner(System.in);
@@ -42,27 +42,27 @@ public class FrequencyAnalysis {
 			char ch = input.next().charAt(0);
 			System.out.println("Enter the character to be replaced with");
 			char newCh = input.next().charAt(0);
-			
+
 			String newText = str.replace(ch, newCh);
 			System.out.println(newText);
 			newText =newText.toUpperCase();
 
 			for(char ch1 ='A';ch1<='Z';ch1++ ) {
-			int n = 0;
-			for(int j=0; j<newText.length(); j++) {
+				int n = 0;
+				for(int j=0; j<newText.length(); j++) {
 
-				if(ch1 == newText.charAt(j))
-					n++;
-	
+					if(ch1 == newText.charAt(j))
+						n++;
+				}
+				System.out.println("Analysis: ");
+				if(n!=0);
+				System.out.print(ch1 + "->" + n + ", ");	
+				}
+			
 			}
-			if(n!=0);
-			System.out.print(ch1 + "->" + n + ", ");	
-		}
-	}
-		else if(Option==2) {
+		if(Option==2) {
 			System.exit(0);
 		}
-		else{
-			System.out.println("Not a valid number!");
-return;
-}}}
+		
+	}
+}
